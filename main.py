2ef118bbfceb66f1156fbc9b56abf2ea31b96b42
rohit340000 <164490543+rohit340000@users.noreply.github.com>
2024-04-16 09:48:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ class Config(object):
     for i in range(len(AUTH_USERS)):
         AUTH_USERS[i] = int(AUTH_USERS[i])
 
-    GROUPS = os.environ.get('GROUPS', '-1001993763284').split(',')
+    GROUPS = os.environ.get('GROUPS', '-1002059340064').split(',')
     for i in range(len(GROUPS)):
         GROUPS[i] = int(GROUPS[i])
 
